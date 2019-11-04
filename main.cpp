3879f2b6481a52c8e8e7cdc1eabed642b963ef82
George Chen <56446026+ghchen99@users.noreply.github.com>
2019-11-04 16:48:46 +0000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,14 +2,6 @@
 #include <fstream>
 #include <iostream>
 
-void add(uint32_t a, uint32_t b){
-  uint32_t c = a + b;
-  std::cout << c << '\n';
-  //r[3] = r[1] + r[2];
-}
-
-//std::vector<uint32_t> r(32);
-
 int main(int argc, char *argv[]){
   std::string binName = argv[1];
   std::cerr << binName; //Cerr for testing, cout actual result
