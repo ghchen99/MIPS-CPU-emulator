05a000aeaba06bcafdbd9855531953c878fd1a3a
Joanna <joannaemerrick@gmail.com>
2019-11-05 21:58:00 +0000

    added noexcept for exceptions, added appropriate headers, removed vector size from variable declaration to class constructor, moved constructor to start of declaration list, fixed exceptions errors, other minor bugfixes

--- cpu.hpp
+++ cpu.hpp
@@ -2,38 +2,49 @@
 #define H_CPU
 
 #include <string>
-#include <cpu.cpp>
+#include <cstdint>
+#include <vector>
+
+#include "instruction.hpp"
 
 class memoryException : public std::exception{
     memoryException(const char *msg) : message(msg) {}
-    const char *what() const{ 
+    const char *what() const noexcept{ 
         return this->message; 
     }
-    const char *msg;
+    const char *message;
 };
 
 class arithmeticException : public std::exception{
     arithmeticException(const char *msg) : message(msg){}
-    const char *what() const{
+    const char *what() const noexcept{
         return this->message; 
     }
-    const char *msg;
-};
-class instructionException : public std::exception{
-    instructionException(const char *msg) : message(msg){}
-    const char *what() const{ 
-        return this->message; 
-    }
-    const char *msg;
+    const char *message;
 };
 
+class instructionException : public std::exception{
+    instructionException(const char *msg) : message(msg){}
+    const char *what() const noexcept{ 
+        return this->message; 
+    }
+    const char *message;
+};
+
+class endException : public std::exception{
+    endException(const char *msg) : message(msg){}
+    const char *what() const noexcept{ 
+        return this->message; 
+    }
+    const char *message;
+};
 
 class CPU{
     private:
-        std::vector <uint8_t> rom(0x1000000); //read only
-        std::vector <uint8_t> ram(0x4000000); //read or write no exectue
-        std::vector <uint32_t> r(32);
-        std::vector <unsigned int> memoryFlags(5);
+        std::vector <uint8_t> rom = std::vector <uint8_t> (0x1000000); //read only
+        std::vector <uint8_t> ram = std::vector <uint8_t> (0x4000000); //read or write no exectue
+        std::vector <uint32_t> r = std::vector <uint32_t> (32);
+        std::vector <unsigned int> memoryFlags = std::vector <unsigned int>(5);
         uint32_t PC;
         uint32_t hi;
         uint32_t lo;
@@ -41,13 +52,13 @@ class CPU{
         instruction nextInstr;
         
     public:
-        void setup();
+        CPU();
+        void run();
         void reset();
         void next();
-        uint32_t AddressMap(uint32_t const &location) const;
-        uint32_t loadInstruction(uint32_t const memLocation) const;
-        CPU();
-}
+        uint32_t addressMap(uint32_t location);
+        uint32_t loadInstruction(uint32_t memLocation);
+};
 
 
 #endif
