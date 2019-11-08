f07473b310540776f427b6f9f3e4b649cb576c9b
George Chen <56446026+ghchen99@users.noreply.github.com>
2019-11-08 16:29:15 +0000

    george created main again

---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include <string>
+#include <fstream>
+#include <iostream>
+#include "cpu.hpp"
+#include "instruction.hpp"
+int main(int argc, char *argv[]){
+  std::string binName = argv[1];
+  std::cerr << binName; //Cerr for testing, cout actual result
+  std::ifstream binStream(argv[1], std::ios::binary);
+
+  CPU mipsCPU;
+
+  binStream.seekg(0, binStream.end);
+  int sizeBin = binStream.tellg();
+  binStream.seekg(0, binStream.beg);
+  std::cout << sizeBin << '\n';
+
+  int count = 0;
+  while(count < 0x1000000){
+    char c;
+    binStream.get(c);
+    /*if(c = NULL){
+    if(c = sizeBin){
+      break;
+    }*/
+    }
+    mipsCPU.loadRom(c, count);
+    count++;
+  }
+
+  int sizeBin = count;
+
+  return 0;
+}
