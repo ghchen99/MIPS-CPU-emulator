663ef74b0cc84da74a745082567f41cb9090c0a7
George Chen <56446026+ghchen99@users.noreply.github.com>
2019-11-21 10:00:02 +0000

    Update testbench.sh

--- test/testbench.sh
+++ test/testbench.sh
@@ -1,5 +1,24 @@
+#!/bin/bash
+
 output="test/output.csv"
 
+run()
+{
+    if [ -f "tests/testcases" ]
+        metadata=()
+        while read
+        
+        testinstruction=metadata[0]
+        expectedreturn=metadata[1]
+        author=metadata[2]
+        
+        RETCODE=$?
+        if [[ "$RETCODE" == expectedreturn" ]] ;
+                echo "Yeet" ;
+        else
+                echo "Neet" ;
+        
+}
 
 bin/simulator tests/addu.bin
 RETCODE=$?
@@ -7,7 +26,7 @@ RETCODE=$?
 if [[ "$RETCODE" == "9" ]] ;
         echo "Success" ;
 else
-        echo "Failure" ;
+        echo "Failure" ; 
         
 bin/simulator testcase/subu.bin
 RETCODE=$?
@@ -16,3 +35,5 @@ if [[ "$RETCODE" == "6" ]] ;
         echo "Success" ;
 else
         echo "Failure" ;
+        
+        how do i comment all of this
