696e59f5847f43ab4fdeac91dc7c03042a56eb76
George Chen <56446026+ghchen99@users.noreply.github.com>
2019-11-21 09:24:23 +0000

    Update bingen.sh

--- test/bingen.sh
+++ test/bingen.sh
@@ -1,27 +1,10 @@
 #!/bin/bash 
 
 srcPath="tests"
-parserPath="./parser/bin/parser"
 metaFiles="./$srcPath/*.meta"
 for meta in $metaFiles
 do
-    $parserPath "$meta" "$srcPath/#"
-done
-
-echo -e "Finished.\n"
-
-
-
-
-
-
-#!/bin/bash
-
-FILES="text_files/*"
-
-for F in $FILES
-do
-    bin/parser $F "binaries/#"
+    ./parser/bin/parser "$meta" "$srcPath/#"
 done
 
 echo -e "Finished.\n"
