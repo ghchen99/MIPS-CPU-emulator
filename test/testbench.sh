34eff507558850a09bf96bc2b9c8e5e930aee7ef
George Chen <56446026+ghchen99@users.noreply.github.com>
2019-11-21 10:39:44 +0000

    Update testbench.sh

--- test/testbench.sh
+++ test/testbench.sh
@@ -5,35 +5,27 @@ output="test/output.csv"
 run()
 {
     if [ -f "tests/testcases" ]
-        metadata=()
-        while read
+        #metadata=()
+        #while read
         
-        testinstruction=metadata[0]
-        expectedreturn=metadata[1]
-        author=metadata[2]
+        #testinstruction=metadata[0]
+        #expectedreturn=metadata[1]
+        #author=metadata[2]
         
-        bin/simulator tests/.bin
+        bin/simulator $1
         
         RETCODE=$?
         if [[ "$RETCODE" == expectedreturn" ]] ;
                 echo "You pass" ;
         else
                 echo "You fail" ;
-        
-bin/simulator tests/addu.bin
-RETCODE=$?
 
-if [[ "$RETCODE" == "9" ]] ;
-        echo "Success" ;
-else
-        echo "Failure" ; 
-        
-bin/simulator testcase/subu.bin
-RETCODE=$?
+}
 
-if [[ "$RETCODE" == "6" ]] ;
-        echo "Success" ;
-else
-        echo "Failure" ;
-        
-        how do i comment all of this
+binfiles="./tests/*.bin"
+for file in binfiles
+do
+    run file
+done
+
+#bin/simulator tests/addu.bin
