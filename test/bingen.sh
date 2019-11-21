60f476a7da2d4060ee4dacf275a6563affcc8dab
George Chen <56446026+ghchen99@users.noreply.github.com>
2019-11-21 12:47:42 +0000

    Update bingen.sh

--- test/bingen.sh
+++ test/bingen.sh
@@ -1,9 +1,9 @@
 #!/bin/bash 
 
-metafiles="./tests/*.meta"
+metafiles="./testcases/*.meta"
 for meta in $metafiles
 do
-    ./parser/bin/parser $meta "tests/#"
+    ./parser/bin/parser $meta "binaries/#"
 done
 
 echo -e "Finished.\n"
